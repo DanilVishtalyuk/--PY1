9552a3b1ce3a5a3d71abf2cb1a49a545a35cc5da
DanilVishtalyuk <115545486+DanilVishtalyuk@users.noreply.github.com>
2022-11-14 17:27:53 +0300

    Update task_3.py

--- лабораторная работа 5/task_3.py
+++ лабораторная работа 5/task_3.py
@@ -1,10 +1,10 @@
 import random
 
 
-def get_unique_list_numbers() -> list[int]:
+def get_unique_list_numbers(min_=-10, max_=10, len_=15) -> list[int]:
     list_unique = []
-    while len(list_unique) != 15:
-        value = random.randint(-10, 10)
+    while len(list_unique) != len_:
+        value = random.randint(min_, max_)
         if value not in list_unique:
             list_unique.append(value)
     return list_unique
@@ -13,5 +13,3 @@ def get_unique_list_numbers() -> list[int]:
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
-
-
